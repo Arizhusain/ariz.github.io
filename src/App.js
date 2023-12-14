79eaa4fc239742dec8295a32107826d5f1934213
Arizhusain <arifhusainsoudagar01@gmail.com>
2023-12-14 19:07:00 +0530

    feat: Add header footer and link font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 }
 
